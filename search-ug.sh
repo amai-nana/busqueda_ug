4f1d46bb875c0fde1515539046fe82174ec10c8a
Tania Marin <taniafcc@gmail.com>
2012-11-07 16:54:41 +0000

    Restructuracion del script, tradado de argumentos con getopts
    Anteriormente el script recibia dos parametros uno de usuario y otro
    de grupo, si no se introducia el usuario hacia la busqueda de "usuario"
    por defecto. Y si no recibia un grupo no importaba.
    
    Se implemento getopts para el trado de las opciones. Ahora al programa
    se le indica que es lo que quier buscar, usuario, grupo o ambos.
    
    Las opciones que checa el programa son:
    
        u para buscar usuario, seguido del nombre de usuario.
        g para buscar grupo, seguido del nombre del grupo.
        a para buscar tanto usuario y grupo.
    
    En caso de que a las opciones no se les pase ningun argumento muestra un
    mensaje de que "se necesitan argumentos", si se escribe una opcion que no
    es valida manda un mensaje diciendo "error de sintaxis" y muestra la forma
    correcta del paso de parametros.

--- search-ug.sh
+++ search-ug.sh
@@ -1,20 +1,57 @@
 #!/bin/bash
 
-var=${1:?"Se necesitan parametros"}
+var=${1:?"Se requieren parametros"}
 
-busqueda=${2:-"usuario"}
+while getopts ":u:g:a" var; do
+    case $var in
+        u) 
+            archivo="/etc/passwd"
+            var=$(egrep "^$OPTARG" ${archivo})
+            if [[ $var ]]; then
+                echo -e "Exite el usuario: $OPTARG en el sistema\n"
+            else
+                echo -e "El usuario $OPTARG no existe en el sistema\n"
+            fi
+            ;;
+        g) 
+            archivo="/etc/group"
+            var=$(egrep "^$OPTARG" ${archivo})
+            if [[ $var ]]; then
+                echo -e "Existe el grupo: $OPTARG en el sistema\n"
+            else
+                echo -e "El usuario $OPTARG no existe en el sistema\n"
+            fi
+            ;;
 
-if [[ $busqueda == "usuario" ]]; then 
-    archivo="/etc/passwd"
-else
-    archivo="/etc/group"
-fi
+        a)
+            shift
+            usuario="/etc/passwd"
+            grupo="/etc/group"
+            var1=$(egrep "^$1" ${usuario})
+            var2=$(egrep "^$2" ${grupo})
+            if [[ $var1 && $var2 ]]; then
+                echo -e "El usuario $1 exixte\n El grupo $2 existe\n"
+            else if [[ !$var1 && $var2 ]]; then
+                    echo -e "El usuario $1 no existe\nEl grupo $2 si existe\n"
+                 else if [[ $var1 && !$var2 ]]; then
+                        echo -e "El usuario $1 si existe\n El grupo $2 no existe\n"
+                     else if [[ !$var1 && !$var2 ]]; then
+                            echo -e "El usuario $1 y el grupo $2 no existen\n"
+                          fi
+                     fi
+                 fi
+            fi
+            ;;
+        \?)
+            echo -e "Error de sintaxis\n"
+            echo "Sintaxis"
+            echo "$0 [-u | -g | -a] [ARGUMENTOS]"
+            echo "-u para buscar usuario"
+            echo "-g para buscar un grupo"
+            echo "-a para buscar un usuario y grupo"
+            ;;
+        :)
+            echo "Se requieren argumentos";;
 
-var=$(egrep "^$var" ${archivo}) 
-
-if [[ $var ]]; then
-    echo -e "Exite : $busqueda: $1 \n:D"
-else 
-    echo -e "No existe $busqueda: $1 \n:("
-fi
-
+    esac
+done

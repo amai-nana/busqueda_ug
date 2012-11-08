101752e23b401573c5465cdabd3681dd541ea619
Tania Marin <taniafcc@gmail.com>
2012-11-08 00:33:16 +0000

    Se agrego licencia al programa

--- search-ug.sh
+++ search-ug.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+## search-ug.sh --- Verificación de existencia de grupo o usuario
+#
+# Filename: search-ug.sh
+# Description: Verificación de existencia de grupo o usuario
+# Author: Tania Marin (nana)
+# Version: 0.1
+# Status: Develop
+#
+# This program is free software# you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation either version 3, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY# without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program# see the file COPYING. If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+#
+######################################################################
+#
+## Code:
+
+# IFS=' \n\t'
+
+#!/bin/bash
+
 var=${1:?"Se requieren parametros"}
 
 while getopts ":u:g:a" var; do

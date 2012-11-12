3ab404789adc9499f11dce9186d5188fe4713217
Tania Marin <taniafcc@gmail.com>
2012-11-12 19:30:32 +0000

    Soporte de localizacion
    Si alguna persona ejecutara el programa y esta no tuviera como idioma
    nativo el español, ahora el script cuenta la traduccion al ingles.

--- search-ug.sh
+++ search-ug.sh
@@ -31,6 +31,9 @@
 
 #!/bin/bash
 
+TEXTDOMAINDIR=/home/aztli/scripts
+TEXTDOMAIN=search-ug
+
 var=${1:?"Se requieren parametros"}
 
 while getopts ":u:g:a" var; do
@@ -39,18 +42,18 @@ while getopts ":u:g:a" var; do
             archivo="/etc/passwd"
             var=$(egrep "^$OPTARG" ${archivo})
             if [[ $var ]]; then
-                echo -e "Exite el usuario: $OPTARG en el sistema\n"
+                echo -e $"Exite el usuario: $OPTARG en el sistema\n"
             else
-                echo -e "El usuario $OPTARG no existe en el sistema\n"
+                echo -e $"El usuario $OPTARG no existe en el sistema\n"
             fi
             ;;
         g) 
             archivo="/etc/group"
             var=$(egrep "^$OPTARG" ${archivo})
             if [[ $var ]]; then
-                echo -e "Existe el grupo: $OPTARG en el sistema\n"
+                echo -e $"Existe el grupo: $OPTARG en el sistema\n"
             else
-                echo -e "El usuario $OPTARG no existe en el sistema\n"
+                echo -e $"El grupo $OPTARG no existe en el sistema\n"
             fi
             ;;
 
@@ -61,13 +64,13 @@ while getopts ":u:g:a" var; do
             var1=$(egrep "^$1" ${usuario})
             var2=$(egrep "^$2" ${grupo})
             if [[ $var1 && $var2 ]]; then
-                echo -e "El usuario $1 exixte\n El grupo $2 existe\n"
+                echo -e $"El usuario $1 existe\n El grupo $2 existe\n"
             else if [[ !$var1 && $var2 ]]; then
-                    echo -e "El usuario $1 no existe\nEl grupo $2 si existe\n"
+                    echo -e $"El usuario $1 no existe\nEl grupo $2 si existe\n"
                  else if [[ $var1 && !$var2 ]]; then
-                        echo -e "El usuario $1 si existe\n El grupo $2 no existe\n"
+                        echo -e $"El usuario $1 si existe\n El grupo $2 no existe\n"
                      else if [[ !$var1 && !$var2 ]]; then
-                            echo -e "El usuario $1 y el grupo $2 no existen\n"
+                            echo -e $"El usuario $1 y el grupo $2 no existen\n"
                           fi
                      fi
                  fi
